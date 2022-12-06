a75f50d49b2e795f6e6ec322e317919a87c57425
indu <70087467+lazyindu@users.noreply.github.com>
2022-12-06 22:36:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/ /MissPerfectURLBOT
+git clone https://github.com/lazyindu/xx /MissPerfectURLBOT
 cd /MissPerfectURLBOT
 pip3 install -r requirements.txt
 echo "Starting Bot...."
